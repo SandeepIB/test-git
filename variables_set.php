3d275be6e7b107da41fc4ddadf3bcde60ca9a320
maazshaikh <maaz.shaikh@infobeans.com>
2016-05-06 13:07:32 +0530

    commit for testing

---
+++ variables_set.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once './includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+db_query("UPDATE users SET NAME = 'admin', pass = MD5('admin123') WHERE uid = 1");
+
+echo "done";
