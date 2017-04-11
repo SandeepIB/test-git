28ad8212651e5822c664f6c35806bad641cc0e5a
Sandeep <sandeep.gupta@infobeans.com>
2017-04-11 16:52:55 +0530

    Revert "Merge branch '910' into stage"
    This reverts commit 82b3a11f9f8f5af2a5f04b5b17ae3d1a3184763c, reversing
    changes made to 0394bf1bed3094f67fc8f3cb10e9850c5905ace4.
    
    Conflicts:
    	variables_set.php

--- variables_set.php
+++ variables_set.php
@@ -8,12 +8,16 @@ db_query("UPDATE users SET NAME = 'admin', pass = MD5('admin123') WHERE uid = 1"
 echo "done";
 
 
+
 911 911 test again
 
 911 testing
 
 
-Test ticket 910 910 
+Test ticket 910
 
+<<<<<<< HEAD
  
 
+=======
+>>>>>>> parent of 82b3a11... Merge branch '910' into stage
